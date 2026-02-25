e21cff35c76d50a6018855b3a7334005408d50d4
MatheusZamo <19matheushenrique03@gmail.com>
2026-02-25 11:10:37 -0300

    Using the intl library for currency formatting

--- challenges/currency-converter-1.0/index.php
+++ challenges/currency-converter-1.0/index.php
@@ -11,16 +11,24 @@
 <body>
     <section>
         <h1>Conversor de Moedas v1.0</h1>
-        <?php 
-        $valueDollar = 5.22;
-        $valueUser = $_GET['currency'];
-        $currency = $valueUser / $valueDollar;
-        $result = round($currency, 2);
+        <p>
+            <?php 
+            $valueDollar = 5.22;
+            $valueUser = $_REQUEST['currency'] ?? 0;
+            $currency = $valueUser / $valueDollar;
 
-        echo "<p>Seus R$ $valueUser equivalem a <strong>US$ $result</strong></p>";
-        echo "<p><strong>*Cotação fixa de R$ $valueDollar</strong> informada diretamente no código</p>";
+            //Codigo sem a biblioteca internacional intl
+            //echo "Seus R\$" . number_format($valueUser, 2, ",", ".") . 
+            //     " equivalem a US\$" . number_format($currency,2, ",", ".");
 
-        ?>
+            //Formatação de moedas com internacionalização
+            //Biblioteca intl (Internallization PHP)
+            $default = numfmt_create("pt-BR", NumberFormatter::CURRENCY);
+
+            echo "Seus " . numfmt_format_currency($default, $valueUser, "BRL") . " equivalem a <strong>" . 
+                numfmt_format_currency($default, $currency, "USD") . "</strong>";
+            ?>
+        </p>
 
         <button onclick="javascript:history.go('-1')">⬅️ Voltar</button>
     </section>
